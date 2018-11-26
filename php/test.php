6e03dac17737dce57f7d2eff024f2765c2e22d79
OdooPlay <root@G3QH-MacBook-Pro-2014.local>
2018-11-26 22:38:58 +0100

    .

--- php/test.php
+++ php/test.php
@@ -3,7 +3,7 @@ $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "employees";
-
+// hi
 $connect =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 if(mysqli_connect_error()){
     echo mysqli_connect_error();
