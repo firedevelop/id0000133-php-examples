e6459b6c2f58eec3f19666f6c14d4af026f39189
OdooPlay <root@G3QH-MacBook-Pro-2014.local>
2018-11-27 23:43:21 +0100

    .

--- php/test.php
+++ php/test.php
@@ -2,25 +2,32 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "employees";
-// hello man
-$connect =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+$db_name = "cms";
+
+$connect = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 if(mysqli_connect_error()){
     echo mysqli_connect_error();
     exit;
 }
-$query = "SELECT * FROM employees";
+$query = "SELECT * FROM article";
 $result = mysqli_query($connect, $query);
-if ($result===false) {
+if($result === false){
     echo mysqli_error($connect);
-}else{
+}else {
     $articles = mysqli_fetch_all($result,MYSQLI_ASSOC);
 }
 ?>
 <!DOCTYPE html>
 <html>
-<body>
-<?php if(empty($articles))?>
+<body> 
+<?php if(empty($articles)): ?>
+	<p>No articles found</p>
+	<?php else:?>
+	<ul>
+	<?php foreach($articles as $article): ?>
+		<li><?= $article['published_at'];?></li>
+		<?php endforeach;?>
+		<?php endif;?>
+	</ul>
 </body>
-
 </html>
