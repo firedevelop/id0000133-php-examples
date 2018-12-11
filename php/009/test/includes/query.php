1ae7bc37b6ca3569da4146b0db44b777c696aa98
firedevelop <firedevelop.com@gmail.com>
2018-12-11 16:23:36 +0100

    .

--- php/009/test/includes/query.php
+++
@@ -1,8 +0,0 @@
-<?php
-$query = "SELECT * FROM article";
-$result = mysqli_query($connect, $query);
-if($result === false){
-    echo mysqli_error($connect);
-}else{
-    $article = mysqli_fetch_all($result,MYSQLI_ASSOC);
-}
