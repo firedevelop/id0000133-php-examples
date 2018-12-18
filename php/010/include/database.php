43c34103557c4d14969d0c319846037190564fe1
firedevelop <firedevelop.com@gmail.com>
2018-12-18 17:23:50 +0100

    .

--- php/010/include/database.php
+++ php/010/include/database.php
@@ -1,4 +1,12 @@
 <?php
 $db_host = "localhost";
+$db_user = "root";
+$db_pass = "123456";
+$db_name = "cms";
 
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if(mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit();
+}
 ?>
